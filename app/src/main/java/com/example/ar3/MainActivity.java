821e02a0177973bdb830265e19b86f076cc2f8ba
Aman Vishwakarma <amanvish54@gmail.com>
2020-09-17 16:00:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ar3/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.ar3;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+    private ArFragment arFragment;
+    private ModelRenderable modelRenderable;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        setUpModel();
+        setUpPlane();
+
+
+
+
+    }
+
+    private void setUpPlane() {
+        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                Anchor anchor = hitResult.createAnchor();
+                AnchorNode anchorNode = new AnchorNode(anchor);
+                anchorNode.setParent(arFragment.getArSceneView().getScene());
+                createModel(anchorNode);
+            }
+        });
+    }
+
+    private void createModel(AnchorNode anchorNode) {
+
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.getScaleController().setMinScale(0.1f);
+        node.setParent(anchorNode);
+
+
+        node.setRenderable(modelRenderable);
+        node.select();
+    }
+
+    private void setUpModel() {
+        ModelRenderable.builder()
+                .setSource(this,R.raw.ironman)
+                .build()
+                .thenAccept(renderable -> modelRenderable = renderable)
+                .exceptionally(throwable -> {
+                    Toast.makeText(getApplicationContext(),"models can't load",Toast.LENGTH_SHORT).show();
+                    return null;
+                });
+
+    }
+
+
+}
